2adc2e4edc62959b8685240cb737934f23b653e9
gartner24 <santiagovalencialeon@gmail.com>
2026-05-07 01:58:22 -0500

    feat(firmware): GPIO trigger + full sketch wiring
    Co-Authored-By: North-Chive <north-chive@users.noreply.github.com>

--- src/attacker/attacker.ino
+++ src/attacker/attacker.ino
@@ -1,8 +1,17 @@
 #include "config.h"
 #include "serial_interface.h"
-#include "control_ap.h"
 #include "api_server.h"
+#include "control_ap.h"
 #include "chain_controller.h"
+#include <Arduino.h>
+
+#define BUTTON_PIN 0  // GPIO 0 (BOOT button on most ESP32 devkits)
+
+static volatile bool _btn_pressed = false;
+
+static void IRAM_ATTR btn_isr() {
+    _btn_pressed = true;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -11,10 +20,18 @@ void setup() {
     serial_interface_init();
     control_ap_start();
     api_server_start();
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), btn_isr, FALLING);
     Serial.println("[INFO] [0] WARDEN ready");
 }
 
 void loop() {
     serial_interface_tick();
+    if (_btn_pressed) {
+        _btn_pressed = false;
+        if (!g_chain_state.ataque_activo && g_config.bssid_validado) {
+            chain_controller_start("cadena_automatica");
+        }
+    }
     delay(10);
 }
